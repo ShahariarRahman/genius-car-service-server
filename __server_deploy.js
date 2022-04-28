14131e51ca2b35b905cc87e495d71ce8c51d0bce
ShahariarRahman <shahariarrahman98@gmail.com>
2022-04-28 22:13:42 +0600

    first commit

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/*
+
+ONE TIME for your computer:
+1. create herocu account
+2. verify email
+3. install heroku cli
+4. heroku login
+
+ONE TIME for each project:
+1. herocu create
+2. git add . git commit git push 
+3. git push heroku main          
+
+*/
---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ massage: 'unauthoruzed access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ massage: 'forbidden access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next()
+    });
+}
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wuayk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
+
+        // require('crypto').randomBytes(64).toString('hex')
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ token });
+            console.log(token);
+        })
+        // service collection api:
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            console.log(newService);
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        // order collection api:
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                return res.status(403).send({ massage: 'forbidden access' })
+            }
+        });
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Running Genius Serverr');
+});
+
+app.listen(port, () => console.log('Listening', port));
